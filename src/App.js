37047be62a00d1d2d4c3eaa05a974bc3dd52ad81
nlee <nlee@energyx.co.kr>
2021-03-23 18:57:09 +0900

    add 리액트 메모, 리듀서 활용하여 성능 최적화

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트 기초', checked: true },
-    { id: 2, text: '컴포넌트 스타일링', checked: true },
-    { id: 3, text: '일정관리 앱 ', checked: false },
-  ]);
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
 
-  const nextId = useRef(4);
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+function App() {
+  /**
+   * 함수실행문 () 으로 초기값 주입하면 리렌더 될 때 마다 createBulkTodo호출
+   * 함수실행문 없이 넣으면 처음 렌더링 될 때만 실행
+   */
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
@@ -19,7 +47,7 @@ function App() {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1;
     },
     [todos],
@@ -27,18 +55,14 @@ function App() {
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      dispatch({ type: 'REMOVE', id });
     },
     [todos],
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      dispatch({ type: 'TOGGLE', id });
     },
     [todos],
   );
